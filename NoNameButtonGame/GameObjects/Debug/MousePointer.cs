96c54dc23e77e536be30ed8e15229da904f33e4e
Erik Schnittker <dev@joyersch.de>
2023-01-20 15:21:32 +0100

    Added missing File since it was moved

---
+++ NoNameButtonGame/GameObjects/Debug/MousePointer.cs
@@ -0,0 +1,29 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using NoNameButtonGame.Hitboxes;
+
+namespace NoNameButtonGame.GameObjects.Debug;
+
+public class MousePointer : GameObject
+{
+    private Vector2 _mousePositionOffset;
+    public Color DrawColor => Color.White;
+
+    public MousePointer()
+    {
+        var textureMapping = Mapping.GetCachedMapping<MousePointer>();
+        Texture = textureMapping.Texture;
+    }
+    public override void Update(GameTime gameTime)
+    {
+        
+        MouseState mouse = Mouse.GetState();
+        _mousePositionOffset = mouse.Position.ToVector2() - new Vector2(3, 3);
+    }
+    
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(Texture, new Rectangle((int)_mousePositionOffset.X, (int)_mousePositionOffset.Y, 6, 6), DrawColor);
+    }
+}
