5164a42b74df2459fedf24c9d1d138f226adb408
Erik Schnittker <dev@joyersch.de>
2023-02-22 18:13:56 +0100

    Updated HoldButtonAddon to stop once it reaches zero

--- NoNameButtonGame/GameObjects/AddOn/HoldButtonAddon.cs
+++ NoNameButtonGame/GameObjects/AddOn/HoldButtonAddon.cs
@@ -9,12 +9,13 @@ namespace NoNameButtonGame.GameObjects.AddOn;
 public class HoldButtonAddon : GameObject
 {
     public event Action TimerReachedZero;
-
-    private float _startTime;
+    
     private readonly EmptyButton _button;
     private readonly TextBuilder _timer;
+    private readonly float _startTime;
     private bool _isHover;
     private float _time;
+    private bool hasReachedZero;
 
     public HoldButtonAddon(EmptyButton button, float startTime) : base(
         button.Position, button.Size, DefaultTexture, DefaultMapping)
@@ -32,14 +33,18 @@ public class HoldButtonAddon : GameObject
     {
         base.Update(gameTime);;
         _button.Update(gameTime, mousePosition);
-        float passedGameTime = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+        float passedGameTime = 0F;
+        if (!hasReachedZero)
+            passedGameTime = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
         if (_isHover && InputReaderMouse.CheckKey(InputReaderMouse.MouseKeys.Left, false))
         {
             _time -= passedGameTime;
             if (_time <= 0)
             {
                 _time = 0;
+                hasReachedZero = true;
                 TimerReachedZero?.Invoke();
+                
             }
         }
         else
@@ -49,7 +54,14 @@ public class HoldButtonAddon : GameObject
                 _time = _startTime;
         }
 
-        _timer.ChangeText((_time / 1000F).ToString("0.0"));
+        string newText = string.Empty;
+        
+        // If _time has no value after decimal point there is no need to print the value after the decimal point
+        if (Math.Floor(_time) == _time)
+            newText = (_time / 1000).ToString("0");
+        else
+            newText = (_time / 1000).ToString("0.0");
+        _timer.ChangeText(newText);
         _timer.Update(gameTime);
     }
 
