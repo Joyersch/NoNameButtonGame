0708d6a84860e78ec04f345db24e5bfdf22564ce
Erik Schnittker <dev@joyersch.de>
2023-09-12 21:51:10 +0200

    Updated Forest and MonoUtils reference

--- NoNameButtonGame/LevelSystem/LevelContainer/Level4/Level.cs
+++ NoNameButtonGame/LevelSystem/LevelContainer/Level4/Level.cs
@@ -86,7 +86,7 @@ public class Level : SampleLevel
         _overworld.GenerateVillages(villageCount);
         _castle = _overworld.GenerateCastle();
         _overworld.GenerateForests(100);
-        _overworld.GenerateTrees(1000);
+        //_overworld.GenerateTrees(1000);
 
         List<ConnectedGameObject> csgos = new List<ConnectedGameObject>();
 
@@ -95,7 +95,7 @@ public class Level : SampleLevel
             for (int y = 0; y < 7; y++)
             {
                 if (!(y == 3 || x == 3) || x <= 1 || x >= 5 || y <= 1 || y >= 5)
-                    csgos.Add(new Forest(new Vector2(x * 32, y * 32), new Vector2(32, 32)));
+                    csgos.Add(new Forest(new Vector2(x * 32, y * 32), new Vector2(32, 32)){DrawColor = Color.Green});
             }
         }
 
