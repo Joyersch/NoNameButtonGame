cedfe9ee4e997a59b0ae42eefad85ae586a60c24
Erik Schnittker <dev@joyersch.de>
2023-12-11 00:28:50 +0100

    Updated Credits-level

--- NoNameButtonGame/LevelSystem/Credits/Level.cs
+++ NoNameButtonGame/LevelSystem/Credits/Level.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using MonoUtils;
@@ -6,6 +8,7 @@ using MonoUtils.Logic;
 using MonoUtils.Logic.Text;
 using MonoUtils.Ui;
 using MonoUtils.Ui.Color;
+using MonoUtils.Ui.Logic;
 using MonoUtils.Ui.Objects;
 using MonoUtils.Ui.Objects.TextSystem;
 
@@ -18,7 +21,42 @@ public class Level : SampleLevel
         var textComponent = TextProvider.GetText("Levels.Credits");
         Name = textComponent.GetValue("Name");
 
-        var cursor = new Cursor();
+        Camera.Zoom = 1F;
+        Camera.Update();
+
+        var credits = Global.ReadFromResources("Text.Credits");
+        string pattern = @"\[(.*?)\]";
+
+        var result = Regex.Replace(
+                credits,
+                pattern,
+                match => textComponent.GetValue(match.Groups[1].Value))
+            .Split("\n");
+
+        float pointer = Camera.Rectangle.Y;
+        Text newLine = null;
+        foreach (string s in result)
+        {
+            newLine = TextNotationProcessor.Parse(s);
+            newLine.GetCalculator(Camera.Rectangle)
+                .OnPositionX(-(Camera.Rectangle.Width / 2) + 16)
+                .OnPositionY(pointer)
+                .ByGridY(1)
+                .Move();
+
+            AutoManaged.Add(newLine);
+            pointer += newLine.Rectangle.Height + Text.DefaultLetterSize.Y / 2;
+        }
+
+        OverTimeMover mover = new OverTimeMover(Camera,
+            new Vector2(Camera.Rectangle.Center.X, newLine.Position.Y + Camera.Rectangle.Height / 2),
+            (pointer + -Camera.Rectangle.Y) * 50, OverTimeMover.MoveMode.Lin);
+
+        mover.ArrivedOnDestination += Exit;
+        mover.Start();
+        AutoManaged.Add(mover);
+
+        var cursor = new Cursor(2F);
         Actuator = cursor;
         PositionListener.Add(Mouse, cursor);
         AutoManaged.Add(cursor);
---
+++ NoNameButtonGame/Text/Credits.md
@@ -0,0 +1,8 @@
+# [Test]
+Wassermelone
+## [Test]
+Wassermelone
+### {#a100ff}[Test]
+Wassermelone{#fc2525}Wassermelone{#76fc3c}Wassermelone{#d6fc3c}Wassermelone
+#### [Test]
+Wassermelone
